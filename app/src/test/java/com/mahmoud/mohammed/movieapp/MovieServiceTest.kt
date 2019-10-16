21dde0703dc56d355146e69719726023f6af9f71
Mohammed Mahmoud <mohammed.95mahmoud@gmail.com>
2019-10-16 18:50:47 +0200

    complete final TDD demo

--- app/src/test/java/com/mahmoud/mohammed/movieapp/MovieServiceTest.kt
+++ app/src/test/java/com/mahmoud/mohammed/movieapp/MovieServiceTest.kt
@@ -1,20 +1,15 @@
 package com.mahmoud.mohammed.movieapp
 
-import com.google.gson.Gson
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import com.mahmoud.mohammed.movieapp.common.Endpoint
-import com.mahmoud.mohammed.movieapp.data.remote.api.MovieListResult
 import com.mahmoud.mohammed.movieapp.data.remote.api.MovieService
-import junit.framework.Assert.assertEquals
 import junit.framework.TestCase
-import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.Rule
 import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.File
 
 const val  jsonResponseFileName="popular_movies_response.json"
 class MovieServiceTestUsingMockWebServer {
@@ -37,7 +32,7 @@ class MovieServiceTestUsingMockWebServer {
 
 
     @Test
-    fun TestMoviesServicePath() {
+    fun testMoviesServicePath() {
         mockWebServer.enqueue(
                 MockResponse()
                         .setBody(MovieTestUtils.getJson(jsonResponseFileName))
